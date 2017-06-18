67f9be0b5a56d8492ca40bead6d5f0b32ad2dc9d
murkey <mhays.dev@gmail.com>
2017-06-18 11:53:44 -0700

    place buildings

--- Assets/MarkerController.cs
+++ Assets/MarkerController.cs
@@ -4,6 +4,7 @@ public class MarkerController : MonoBehaviour {
     public Collider coll;
 
     private Renderer rend;
+    private bool canBuild;
 
     void Start() {
         rend = GetComponent<Renderer>();
@@ -15,11 +16,21 @@ public class MarkerController : MonoBehaviour {
         if (coll.Raycast(ray, out hit, 2000)) {
             Vector3 pos = ray.GetPoint(hit.distance);
             transform.position = pos;
-            if (pos.y > 0) {
+            canBuild = pos.y > 0;
+            if (canBuild) {
                 rend.material.color = Color.white;
             } else {
                 rend.material.color = Color.red;
             }
+        }
+
+        if (Input.GetMouseButtonDown(0) && canBuild) {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = transform.position;
+            cube.transform.rotation = transform.rotation;
+            cube.transform.localScale = transform.localScale;
+            Renderer cubeRend = cube.GetComponent<Renderer>();
+            cubeRend.material.CopyPropertiesFromMaterial(rend.material);
         }
     }
 }
