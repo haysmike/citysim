f96d393e34ecefa5c1a9e03197fb14a392e79b25
murkey <mhays.dev@gmail.com>
2017-06-17 23:04:24 -0700

    init

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Camera.main.fieldOfView -= 20 * scroll;
+    }
+}
---
+++ Assets/MarkerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MarkerController : MonoBehaviour {
+    public Transform marker;
+    public Collider coll;
+
+    private Renderer rend;
+
+    void Start() {
+        rend = marker.GetComponent<Renderer>();
+    }
+
+    void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (coll.Raycast(ray, out hit, 2000)) {
+            Vector3 pos = ray.GetPoint(hit.distance);
+            marker.position = pos;
+            if (pos.y > 0) {
+                rend.material.color = Color.white;
+            } else {
+                rend.material.color = Color.red;
+            }
+        }
+    }
+}
