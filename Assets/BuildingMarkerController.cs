88d0dd1f022db2c113acd96cefcf8ce892d4d2b9
murkey <mhays.dev@gmail.com>
2017-06-20 22:14:13 -0700

    building prefab, can gen terrain on play

---
+++ Assets/BuildingMarkerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class BuildingMarkerController : MonoBehaviour {
+    public Collider terrainCollider;
+    public GameObject buildingPrefab;
+
+    private Renderer rend;
+    private int numCollisions;
+
+    void Start() {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (!terrainCollider.Raycast(ray, out hit, 2000)) return;
+
+        Vector3 pos = ray.GetPoint(hit.distance);
+        transform.position = pos;
+
+        bool canBuild = pos.y > 0 && numCollisions == 0;
+        if (canBuild) {
+            rend.material.color = Color.white;
+        } else {
+            rend.material.color = Color.red;
+        }
+
+        if (Input.GetMouseButtonDown(0) && canBuild) {
+            CreateBuilding();
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            transform.localScale = new Vector3(Random.Range(4, 12), Random.Range(8, 18), Random.Range(4, 12));
+            transform.rotation = Quaternion.Euler(0.0f, Random.Range(0.0f, 180.0f), 0.0f);
+        }
+    }
+
+    void OnTriggerEnter(Collider coll) {
+        numCollisions++;
+    }
+
+    void OnTriggerExit(Collider coll) {
+        numCollisions--;
+    }
+
+    void CreateBuilding() {
+        GameObject building = Instantiate(buildingPrefab, transform.position, transform.rotation);
+        building.transform.localScale = transform.localScale;
+        Renderer buildingRenderer = building.GetComponent<Renderer>();
+        buildingRenderer.material.color = Random.ColorHSV();
+    }
+}
---
+++ Assets/TerrainController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+using com.heparo.terrain.toolkit;
+
+public class TerrainController : MonoBehaviour {
+    private TerrainToolkit toolkit;
+    private Terrain terrain;
+
+    void Start() {
+        terrain = GetComponent<Terrain>();
+        if (terrain == null) return;
+
+        toolkit = GetComponent<TerrainToolkit>();
+        if (toolkit == null) return;
+
+        // Thread thread = new Thread(GenerateTerrain);
+        // thread.Start();
+        GenerateTerrain();
+    }
+
+    void GenerateTerrain() {
+        terrain.enabled = false;
+        // if (toolkit.heightBlendPoints == null) {
+        //     Debug.Log("Setting height blend points");
+        //     toolkit.heightBlendPoints = new List<float>();
+        // }
+        toolkit.setPerlinPreset(new TerrainToolkit.perlinPresetData("", 7, 0.5f, 4, 1.0f));
+        toolkit.generateTerrain(onProgressUpdate);
+        toolkit.textureTerrain(onProgressUpdate);
+        toolkit.setFullHydraulicErosionPreset(new TerrainToolkit.fullHydraulicErosionPresetData("", 25, 0.01f, 0.5f, 0.06f, 0.6f));
+        toolkit.erodeAllTerrain(onErosionProgressUpdate);
+        // toolkit.erodeAllTerrain(onErosionProgressUpdate);
+        terrain.enabled = true;
+    }
+
+    void onProgressUpdate(string titleString, string displayString, float percentComplete) {
+        Debug.Log(titleString + " - " + displayString + " " + percentComplete +"%");
+    }
+
+    public void onErosionProgressUpdate(string titleString, string displayString, int iteration, int nIterations, float percentComplete ){
+        onProgressUpdate(titleString, displayString+" Iteration "+iteration+" of "+nIterations+". Please wait.", percentComplete);
+    }
+}
